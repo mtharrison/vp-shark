a3a95434f51f09dce92ce020190e0628bd52c66c
Matt Harrison <mt.harrison86@gmail.com>
2014-07-26 16:22:55 +0100

    Started building the Shark.Game interface

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Shark = require('./lib/shark');
 
 // Load modules
 Shark.Server = require('./lib/server');
-Shark.game   = require('./lib/game');
+Shark.Game   = require('./lib/game');
 Shark.Tester = require('./lib/tester');
 
 module.exports = Shark;
